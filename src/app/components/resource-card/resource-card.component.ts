d0c6b05634d0652a3cbe2dc9239738d755f99fdc
Han Han <haannn@qq.com>
2020-08-11 19:42:08 +0800

    feat: 点击card跳转

--- src/app/components/resource-card/resource-card.component.ts
+++ src/app/components/resource-card/resource-card.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ElementRef,
+  AfterViewInit,
+  HostListener,
+} from '@angular/core';
 import { Resource } from 'src/app/interfaces/resource.interface';
 
 @Component({
@@ -6,11 +13,27 @@ import { Resource } from 'src/app/interfaces/resource.interface';
   templateUrl: './resource-card.component.html',
   styleUrls: ['./resource-card.component.less'],
 })
-export class ResourceCardComponent implements OnInit {
+export class ResourceCardComponent implements OnInit, AfterViewInit {
   @Input() resource: Resource;
-  constructor() {}
+  imgUrl: string;
 
-  ngOnInit(): void {}
+  constructor(private elementRef: ElementRef) {}
+
+  @HostListener('click', ['$event']) onClick(event) {
+    // console.log('component is clicked');
+    window.open(this.resource.link, '_blank');
+  }
+  ngOnInit(): void {
+    this.imgUrl = this.randomPic();
+  }
+  ngAfterViewInit(): void {
+    // this.elementRef.nativeElement
+    // .
+    //   .addEventListener('click', this.onClick.bind(this));
+  }
+  // onClick(e) {
+  //   console.log(e);
+  // }
   randomPic(): string {
     let num = ~~(Math.random() * 100);
     return `https://picsum.photos/200/140?random=${num}`;
